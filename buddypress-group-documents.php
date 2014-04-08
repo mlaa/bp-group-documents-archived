62d7f595242fa6158bacc3b31a682bfe398bba7e
Jonathan Reeve <jon.reeve@gmail.com>
2014-04-08 10:38:08 -0400

    moved debugging functions

--- buddypress-group-documents.php
+++ buddypress-group-documents.php
@@ -381,19 +381,3 @@ if (class_exists('BP_Group_Extension')) : // Recommended, to prevent problems du
 
 endif; // class_exists( 'BP_Group_Documents_Extension' )
 
-// Debugging logs. -JR
-if (!function_exists('write_log')) {
-	function write_log ( $log )  {
-		if ( true === WP_DEBUG ) {
-			if ( is_array( $log ) || is_object( $log ) ) {
-				error_log( print_r( $log, true ) );
-			} else {
-				error_log( $log );
-			}
-		}
-	}
-}
-
-write_log("A test log message."); 
-
-
